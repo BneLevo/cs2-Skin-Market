33d4ce6869d3758883ed488a3d116b38a719e2ae
levent-ozgn <levent.ozgn@eduge.ch>
2026-02-23 21:10:06 +0100

    mongodb connection

--- cs2-skins-market/Form1.cs
+++ cs2-skins-market/Form1.cs
@@ -1,3 +1,5 @@
+using MongoDB.Driver;
+
 namespace cs2_skins_market
 {
     public partial class Form1 : Form
@@ -5,6 +7,27 @@ namespace cs2_skins_market
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+                var client = MongoDBconnect.Instance();
+                var databases = client.ListDatabaseNames().ToList();
+
+
+                string allDb = string.Join(", ", databases);
+
+                MessageBox.Show(allDb);
+                MessageBox.Show("T'es trop fort");
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Could not connected to mongodb");
+            }
         }
     }
 }
---
+++ cs2-skins-market/MongoDBconnect.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace cs2_skins_market
+{
+    public sealed class MongoDBconnect
+    {
+
+        public MongoDBconnect(){ }
+
+        private static MongoClient _instance;
+        private static readonly object _lock = new object();
+         
+        public static MongoClient Instance()
+        {
+            if (_instance == null)
+            {
+
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new MongoClient("mongodb://localhost:27018");
+                    }
+                }
+
+            }
+
+            return _instance;
+        }
+
+
+
+    }
+}

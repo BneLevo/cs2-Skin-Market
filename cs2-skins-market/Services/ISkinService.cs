cbbca6c0b8dbda1ace5d96b71c9dd00166839248
levent-ozgn <levent.ozgn@eduge.ch>
2026-02-25 17:39:25 +0100

    skin manager

---
+++ cs2-skins-market/Services/ISkinService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using cs2_skins_market.Models;
+
+namespace cs2_skins_market.Services
+{
+    interface ISkinService
+    {
+        List<Skin> GetAllSkins();
+        //List<Skin> GetByCountry(string category);
+    }
+}

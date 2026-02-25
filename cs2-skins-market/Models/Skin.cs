fda8fa327dce226ed3e50afdf86f81c7fe84d22a
levent-ozgn <levent.ozgn@eduge.ch>
2026-02-25 17:12:27 +0100

    a class for skins

---
+++ cs2-skins-market/Models/Skin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace cs2_skins_market.Models
+{
+    public class Skin
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("img")]
+        public string Img { get; set; }
+
+        [BsonElement("price")]
+        public double Price { get; set; }
+
+        [BsonElement("item")]
+        public string Item { get; set; }
+
+        [BsonElement("skinName")]
+        public string SkinName { get; set; }
+    }
+}
